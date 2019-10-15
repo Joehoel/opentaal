2a48cbeff17cae9ae05ac51e56f7f08fc5342fee
Joehoel <jmkuijper@Outlook.com>
2019-10-15 17:08:31 +0200

    Made it better

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.readFile('./databases/OpenTaal-210G-basis-gekeurd.txt', (err, data) => {
+fs.readFile("./databases/OpenTaal-210G-basis-gekeurd.txt", (err, data) => {
   if (err) throw err;
-  data += '';
+  data += "";
 
-  let words = data.split('\n');
+  let words = data.split("\n");
 
   function getRandomWord() {
-    return words[Math.floor(Math.random() * words.length)];
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    if (randomWord.length > 4) {
+      console.log(randomWord);
+    }
   }
-
-  const randomWord = getRandomWord();
-
-  console.log(randomWord);
+  getRandomWord();
 });
